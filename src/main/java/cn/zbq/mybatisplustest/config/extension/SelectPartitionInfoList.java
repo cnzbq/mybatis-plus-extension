017198f2e55e65dc10b2b3ee1d6a3444f5728cca
zbq2019 <49929998+zbq2019@users.noreply.github.com>
2022-07-05 08:56:07 +0800

    Update SelectPartitionInfoList.java
    优化代码

--- src/main/java/cn/zbq/mybatisplustest/config/extension/SelectPartitionInfoList.java
+++ src/main/java/cn/zbq/mybatisplustest/config/extension/SelectPartitionInfoList.java
@@ -16,7 +16,7 @@ import org.apache.ibatis.mapping.SqlSource;
  * @since 2022/7/4
  */
 public class SelectPartitionInfoList extends AbstractMethod {
-    private static final String sqlScript = "<script>\nselect %s from information_schema.partitions %s %s %s\n</script>";
+    private static final String SQL_SCRIPT = "<script>\nselect %s from information_schema.partitions %s %s %s\n</script>";
 
     public SelectPartitionInfoList() {
         super("selectPartitionInfoList");
@@ -30,25 +30,25 @@ public class SelectPartitionInfoList extends AbstractMethod {
         TableInfo info = TableInfoHelper.initTableInfo(builderAssistant, PartitionInfo.class);
         String sqlSelect = info.getAllSqlSelect();
 
-        String sql = String.format(sqlScript, sqlSelect, sqlWhereEntityWrapper(info, tableName), sqlOrderBy(tableInfo), sqlComment());
+        String sql = String.format(SQL_SCRIPT, sqlSelect, sqlWhereEntityWrapper(info, tableName), sqlOrderBy(info), sqlComment());
         SqlSource sqlSource = languageDriver.createSqlSource(configuration, sql, modelClass);
         return this.addSelectMappedStatementForOther(mapperClass, sqlSource, PartitionInfo.class);
     }
 
     private String sqlWhereEntityWrapper(TableInfo table, String tableName) {
-        String sqlScript = table.getAllSqlWhere(false, true, WRAPPER_ENTITY_DOT);
-        sqlScript = SqlScriptUtils.convertIf(sqlScript, String.format("%s != null", WRAPPER_ENTITY), true);
-        sqlScript += NEWLINE;
-        sqlScript += SqlScriptUtils.convertIf(String.format("AND ${%s}", WRAPPER_SQLSEGMENT),
+        String sqlScript1 = table.getAllSqlWhere(false, true, WRAPPER_ENTITY_DOT);
+        sqlScript1 = SqlScriptUtils.convertIf(sqlScript1, String.format("%s != null", WRAPPER_ENTITY), true);
+        sqlScript1 += NEWLINE;
+        sqlScript1 += SqlScriptUtils.convertIf(String.format("AND ${%s}", WRAPPER_SQLSEGMENT),
                 String.format("%s != null and %s != '' and %s", WRAPPER_SQLSEGMENT, WRAPPER_SQLSEGMENT, WRAPPER_NONEMPTYOFWHERE), true);
-        sqlScript = SqlScriptUtils.convertIf(sqlScript, String.format("%s != null", WRAPPER), true);
-        sqlScript = String.format("table_schema= schema() AND table_name= '%s'", tableName) + NEWLINE + sqlScript;
-        sqlScript = SqlScriptUtils.convertWhere(sqlScript) + NEWLINE;
+        sqlScript1 = SqlScriptUtils.convertIf(sqlScript1, String.format("%s != null", WRAPPER), true);
+        sqlScript1 = String.format("table_schema= schema() AND table_name= '%s'", tableName) + NEWLINE + sqlScript1;
+        sqlScript1 = SqlScriptUtils.convertWhere(sqlScript1) + NEWLINE;
 
         String sqlScript2 = SqlScriptUtils.convertIf(String.format(" ${%s}", WRAPPER_SQLSEGMENT),
                 String.format("%s != null and %s != '' and %s", WRAPPER_SQLSEGMENT, WRAPPER_SQLSEGMENT,
                         WRAPPER_EMPTYOFWHERE), true);
         sqlScript2 = SqlScriptUtils.convertIf(sqlScript2, String.format("%s != null", WRAPPER), true);
-        return sqlScript + NEWLINE + sqlScript2;
+        return sqlScript1 + NEWLINE + sqlScript2;
     }
 }
